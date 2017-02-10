67e0f42d4b76a3588b62849149ef6d7180368d20
Cesar Varela <mail@cesarvarela.com>
2017-02-10 15:15:34 -0300

    Lots of cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
-export class WatsonRecognizer {
+import { IIntentRecognizer, IIntentRecognizerResult, IIntent, IEntity } from 'botbuilder'
 
-    models: {}
+export class WatsonRecognizer implements IIntentRecognizer {
+
     conversation: any;
 
     constructor(private username: string, private password: string, private workspace: string) {
-        
+
         this.conversation = new ConversationV1({
             username: username,
             password: password,
@@ -14,85 +15,41 @@ export class WatsonRecognizer {
         });
     }
 
-    public recognize(context, cb) {
+    recognize(context, callback) {
 
-        var result: any = { score: 0.0, intent: null };
+        var result: IIntentRecognizerResult = { score: 0.0, intent: null };
 
         if (context && context.message && context.message.text) {
 
-            var utterance = context.message.text;
-
-            WatsonRecognizer.recognize(utterance, function (err, intents: any, entities) {
+            this.conversation.message({
+                input: { text: context.message.text },
+                workspace_id: this.workspace
+            }, (err, response) => {
 
                 if (!err) {
-                    result.intents = intents;
-                    result.entities = entities;
-                    var top;
-                    intents.forEach(function (intent) {
-                        if (top) {
-                            if (intent.score > top.score) {
-                                top = intent;
-                            }
-                        }
-                        else {
-                            top = intent;
-                        }
-                    });
-                    if (top) {
-                        result.score = top.score;
-                        result.intent = top.intent;
-                        switch (top.intent.toLowerCase()) {
-                            case 'builtin.intent.none':
-                            case 'none':
-                                result.score = 0.1;
-                                break;
-                        }
-                    }
-                    cb(null, result);
+
+                    // map entities to botbuilder format
+
+                    result.entities = (response.entities as Array<any>).map<IEntity>(e => ({ type: e.entity, entity: e.value, startIndex: e.location[0], endIndex: e.location[1] }))
+
+                    // map intents to botbuilder format
+
+                    result.intents = (response.intents as Array<{ intent, confidence }>).map<IIntent>(i => ({ intent: i.intent, score: i.confidence }))
+
+                    let top = result.intents.sort((a, b) => a.score - b.score)[0]
+
+                    result.score = top.score;
+                    result.intent = top.intent;
+
+                    callback(null, result)
                 }
                 else {
-                    cb(err, null);
+                    callback(err, null);
                 }
-
-            }, this.conversation, this.workspace);
+            })
         }
         else {
-            cb(null, result);
+            callback(null, result);
         }
     }
-
-    //TODO: check why this is an static function, if is not used as such, make it a local function
-
-    static recognize(utterance, callback, conversation, workspace) {
-
-        try {
-
-            conversation.message({
-                input: { text: utterance },
-                workspace_id: workspace
-            }, (err, response) => {
-               
-                try {
-                    if (!err) {
-                        
-                        let intents = (response.intents as Array<{intent, confidence}>) .map( i => ({ intent: i.intent, score:  i.confidence}))
-                        let entities = response.entities.map(e => ({entity: e.value, type: e.entity}))
-
-                        callback(null, intents, entities);
-                    }
-                    else {
-                        var m = err.toString();
-                        callback(err instanceof Error ? err : new Error(m));
-                    }
-                }
-                catch (e) {
-                    console.error(e.toString());
-                }
-            });
-        }
-        catch (err) {
-            callback(err instanceof Error ? err : new Error(err.toString()));
-        }
-    }
-
 }
