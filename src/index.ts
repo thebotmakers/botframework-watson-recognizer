94757a547b6843a2ef0054acd841bda76934763e
Pablo Costa <pablocostapossamai@gmail.com>
2017-03-31 21:38:44 -0300

    Add Watson multilanguage models support

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
+import * as _ from 'lodash';
 import { IIntentRecognizer, IIntentRecognizerResult, IIntent, IEntity } from 'botbuilder'
+
+export interface IWatsonModel {
+    username: string;
+    password: string;
+    workspaceId: string;
+}
+
+export interface IWatsonModelMap {
+    [local: string]: IWatsonModel;
+}
+
+interface ConversationModel {
+    workspaceId: string;
+    conversation: any;
+}
 
 export class WatsonRecognizer implements IIntentRecognizer {
 
-    conversation: any;
     onRecognizeCallback: any;
+    private conversationModels: _.Map<ConversationModel>;
     private intentThreshold: number;
 
-    constructor(private username: string, private password: string, private workspace: string, intentThreshold: number) {
+    constructor(private models: IWatsonModelMap, intentThreshold: number) {
 
+        debugger;
         this.intentThreshold = intentThreshold;
-        this.conversation = new ConversationV1({
-            username: username,
-            password: password,
-            version_date: ConversationV1.VERSION_DATE_2016_09_20
-        });
 
+        this.conversationModels = [];
+        _.each(models, (model, key) => {
+            debugger;
+            var conversation = new ConversationV1({
+                username: model.username,
+                password: model.password,
+                version_date: ConversationV1.VERSION_DATE_2016_09_20
+            });
+            this.conversationModels[key] = {
+                workspaceId: model.workspaceId,
+                conversation: conversation
+            };
+        });
     };
 
     setCallback(onRecognizeCallback: any) {
@@ -34,41 +59,53 @@ export class WatsonRecognizer implements IIntentRecognizer {
         var result: IIntentRecognizerResult = { score: 0.0, intent: null };
 
         if (context && context.message && context.message.text) {
-            let textClean = context.message.text.replace(/(\r\n|\n|\r)/gm, " ")
-            this.conversation.message({
-                input: { text: textClean },
-                workspace_id: this.workspace
-            }, (err, response) => {
+            let textClean = context.message.text.replace(/(\r\n|\n|\r)/gm, " ");
 
-                if (!err) {
+            // get Locale model on Watson
+            var locale = context.locale || 'es-ES';
+            locale = _.split(locale, '-', 1);
+            var conversationModel = this.conversationModels[locale];// ? this.conversationModels[locale] : this.models['*'];
 
-                    // map entities to botbuilder format
+            debugger;
+            if (conversationModel) {
+                conversationModel.conversation.message({
+                    input: { text: textClean },
+                    workspace_id: conversationModel.workspaceId
+                }, (err, response) => {
 
-                    result.entities = (response.entities as Array<any>).map<IEntity>(e => ({ type: e.entity, entity: e.value, startIndex: e.location[0], endIndex: e.location[1] }))
+                    if (!err) {
 
-                    // map intents to botbuilder format
+                        // map entities to botbuilder format
 
-                    result.intents = (response.intents as Array<any>).map<IIntent>(i => ({ intent: i.intent, score: i.confidence }))
+                        result.entities = (response.entities as Array<any>).map<IEntity>(e => ({ type: e.entity, entity: e.value, startIndex: e.location[0], endIndex: e.location[1] }))
 
-                    let top = result.intents.sort((a, b) => a.score - b.score)[0]
+                        // map intents to botbuilder format
 
-                    //filter intents with less than intentThreshold
-                    result.score = top.score < this.intentThreshold ? 0 : top.score;
-                    result.intent = top.intent;
+                        result.intents = (response.intents as Array<any>).map<IIntent>(i => ({ intent: i.intent, score: i.confidence }))
 
-                    //Add intent and score to message object
-                    context.message.intent = result.intent;
-                    context.message.score = result.score;
+                        let top = result.intents.sort((a, b) => a.score - b.score)[0]
 
-                    if (this.onRecognizeCallback) {
-                        this.onRecognizeCallback(context);
+                        //filter intents with less than intentThreshold
+                        result.score = top.score < this.intentThreshold ? 0 : top.score;
+                        result.intent = top.intent;
+
+                        //Add intent and score to message object
+                        context.message.intent = result.intent;
+                        context.message.score = result.score;
+
+                        if (this.onRecognizeCallback) {
+                            this.onRecognizeCallback(context);
+                        }
+                        callback(null, result);
                     }
-                    callback(null, result);
-                }
-                else {
-                    callback(err, null);
-                }
-            })
+                    else {
+                        callback(err, null);
+                    }
+                })
+            }
+            else {
+                callback(new Error("Watson model not found for locale '" + locale + "'."), null);
+            }
         }
         else {
             callback(null, result);
