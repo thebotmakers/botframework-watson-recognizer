01a67d600032e196c84f705f549661fb394a68d3
Pablo Costa <pablocostapossamai@gmail.com>
2017-03-31 21:48:40 -0300

    Add dist folder to git

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,18 @@
 import { IIntentRecognizer } from 'botbuilder';
+export interface IWatsonModel {
+    username: string;
+    password: string;
+    workspaceId: string;
+}
+export interface IWatsonModelMap {
+    [local: string]: IWatsonModel;
+}
 export declare class WatsonRecognizer implements IIntentRecognizer {
-    private username;
-    private password;
-    private workspace;
-    conversation: any;
+    private models;
     onRecognizeCallback: any;
+    private conversationModels;
     private intentThreshold;
-    constructor(username: string, password: string, workspace: string, intentThreshold: number);
+    constructor(models: IWatsonModelMap, intentThreshold: number);
     setCallback(onRecognizeCallback: any): void;
     recognize(context: any, callback: any): void;
 }
--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,24 @@
 "use strict";
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+var _ = require('lodash');
 var WatsonRecognizer = (function () {
-    function WatsonRecognizer(username, password, workspace, intentThreshold) {
-        this.username = username;
-        this.password = password;
-        this.workspace = workspace;
+    function WatsonRecognizer(models, intentThreshold) {
+        var _this = this;
+        this.models = models;
+        debugger;
         this.intentThreshold = intentThreshold;
-        this.conversation = new ConversationV1({
-            username: username,
-            password: password,
-            version_date: ConversationV1.VERSION_DATE_2016_09_20
+        this.conversationModels = [];
+        _.each(models, function (model, key) {
+            debugger;
+            var conversation = new ConversationV1({
+                username: model.username,
+                password: model.password,
+                version_date: ConversationV1.VERSION_DATE_2016_09_20
+            });
+            _this.conversationModels[key] = {
+                workspaceId: model.workspaceId,
+                conversation: conversation
+            };
         });
     }
     ;
@@ -26,31 +35,41 @@ var WatsonRecognizer = (function () {
         var result = { score: 0.0, intent: null };
         if (context && context.message && context.message.text) {
             var textClean = context.message.text.replace(/(\r\n|\n|\r)/gm, " ");
-            this.conversation.message({
-                input: { text: textClean },
-                workspace_id: this.workspace
-            }, function (err, response) {
-                if (!err) {
-                    // map entities to botbuilder format
-                    result.entities = response.entities.map(function (e) { return ({ type: e.entity, entity: e.value, startIndex: e.location[0], endIndex: e.location[1] }); });
-                    // map intents to botbuilder format
-                    result.intents = response.intents.map(function (i) { return ({ intent: i.intent, score: i.confidence }); });
-                    var top_1 = result.intents.sort(function (a, b) { return a.score - b.score; })[0];
-                    //filter intents with less than intentThreshold
-                    result.score = top_1.score < _this.intentThreshold ? 0 : top_1.score;
-                    result.intent = top_1.intent;
-                    //Add intent and score to message object
-                    context.message.intent = result.intent;
-                    context.message.score = result.score;
-                    if (_this.onRecognizeCallback) {
-                        _this.onRecognizeCallback(context);
+            // get Locale model on Watson
+            var locale = context.locale || 'es-ES';
+            locale = _.split(locale, '-', 1);
+            var conversationModel = this.conversationModels[locale]; // ? this.conversationModels[locale] : this.models['*'];
+            debugger;
+            if (conversationModel) {
+                conversationModel.conversation.message({
+                    input: { text: textClean },
+                    workspace_id: conversationModel.workspaceId
+                }, function (err, response) {
+                    if (!err) {
+                        // map entities to botbuilder format
+                        result.entities = response.entities.map(function (e) { return ({ type: e.entity, entity: e.value, startIndex: e.location[0], endIndex: e.location[1] }); });
+                        // map intents to botbuilder format
+                        result.intents = response.intents.map(function (i) { return ({ intent: i.intent, score: i.confidence }); });
+                        var top_1 = result.intents.sort(function (a, b) { return a.score - b.score; })[0];
+                        //filter intents with less than intentThreshold
+                        result.score = top_1.score < _this.intentThreshold ? 0 : top_1.score;
+                        result.intent = top_1.intent;
+                        //Add intent and score to message object
+                        context.message.intent = result.intent;
+                        context.message.score = result.score;
+                        if (_this.onRecognizeCallback) {
+                            _this.onRecognizeCallback(context);
+                        }
+                        callback(null, result);
                     }
-                    callback(null, result);
-                }
-                else {
-                    callback(err, null);
-                }
-            });
+                    else {
+                        callback(err, null);
+                    }
+                });
+            }
+            else {
+                callback(new Error("Watson model not found for locale '" + locale + "'."), null);
+            }
         }
         else {
             callback(null, result);
